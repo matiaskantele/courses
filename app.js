801234ee69847bc166a73054718a36485e75d7b6
Matias Kantele <matias.kantele@siili.com>
2020-01-03 00:39:27 +0200

    add controllers for products and 404 page

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const notFoundController = require("./controllers/notFound");
 
 const app = express();
 
@@ -13,11 +15,9 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(notFoundController.get404);
 
 app.listen(3000);
