963769eac4b73068bdb2417edcfc7201efc30745
Matias Kantele <matias.kantele@siili.com>
2020-01-02 23:23:41 +0200

    favorite pug as template engine

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
