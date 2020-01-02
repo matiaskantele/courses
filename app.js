465a7a2d3a2b2d7689adb54e221951b3970378a0
Matias Kantele <matias.kantele@siili.com>
2020-01-02 23:07:10 +0200

    give ejs a try as a templating engine

--- app.js
+++ app.js
@@ -2,23 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  exphbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: ".hbs",
-  })
-);
-app.set("view engine", "hbs");
-// app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
