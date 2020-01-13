0769dbead67635d659cc10a0dcc532d4a1bb4343
Matias Kantele <matias.kantele@siili.com>
2020-01-13 23:08:04 +0200

    add delete product functionality for admin view

--- models/cart.js
+++ models/cart.js
@@ -13,9 +13,7 @@ module.exports = class Cart {
         cart = JSON.parse(content);
       }
       // Analyze cart => Find existing product
-      const existingProductIndex = cart.products.findIndex(
-        prod => prod.id === id
-      );
+      const existingProductIndex = cart.products.findIndex(p => p.id === id);
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
       // Add new product / increase quantity
@@ -30,6 +28,21 @@ module.exports = class Cart {
       }
       cart.totalPrice += +productPrice;
       fs.writeFile(p, JSON.stringify(cart), err => console.log(err));
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, content) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const updatedCart = { ...JSON.parse(content) };
+      const product = updatedCart.products.find(p => p.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(p => p.id !== id);
+      updatedCart.totalPrice -= productPrice * productQty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => console.log(err));
     });
   }
 };
--- models/product.js
+++ models/product.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-const rootDir = require("../util/path");
 
+const rootDir = require("../util/path");
 const p = path.join(rootDir, "data", "products.json");
+const Cart = require("./cart");
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, content) => {
@@ -44,6 +45,18 @@ module.exports = class Product {
           }
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
