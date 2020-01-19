59352b2aaf7170bea49ea7c66f33f6c03c3b0adf
Matias Kantele <matias.kantele@siili.com>
2020-01-19 12:47:26 +0200

    use sequelize for the rest of product management

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,8 +33,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -70,7 +72,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render("admin/product-list", {
         prods: products,
