b3b6b6b10f90464fb417a9ec34405e76f520fef7
Matias Kantele <matias.kantele@siili.com>
2020-01-22 23:37:14 +0200

    get user for requests

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const notFoundController = require('./controllers/notFound');
-// const User = require("./models/user");
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("5e27b9371c9d440000abd8cd")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5e28be82532a6c8d06f5e534')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +39,16 @@ const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose
   .connect(uri, options)
   .then(() => {
+    User.findOne().then(user => {
+      if (!user) {
+        const newUser = new User({
+          name: 'Matias',
+          email: 'matias@test.com',
+          cart: [],
+        });
+        newUser.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => console.log(err));
