d4907494be9fca88be5fd883dbb451a3dbfde996
Matias Kantele <matias.kantele@siili.com>
2020-01-22 05:03:37 +0200

    link product to a user in mongodb

--- models/user.js
+++ models/user.js
@@ -1,22 +1,22 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const getDb = require("../util/db").getDb;
 
-const sequelize = require("../util/db");
+class User {
+  constructor(username, email) {
+    (this.name = username), (this.email = email);
+  }
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) });
+  }
+}
 
 module.exports = User;
