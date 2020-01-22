d534554761579865e2ac243522fd1cdeedc6c1cc
Matias Kantele <matias.kantele@siili.com>
2020-01-22 05:37:10 +0200

    add products to cart

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 const mongodb = require("mongodb");
 const getDb = require("../util/db").getDb;
 
+const ObjectId = mongodb.ObjectId;
+
 class User {
-  constructor(username, email, cart) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
     this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -13,11 +16,36 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
-  static findById(userId) {
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(
+      item => item.productId.toString() === product._id.toString()
+    );
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex !== -1) {
+      updatedCartItems[cartProductIndex].quantity += 1;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
     const db = getDb();
     return db
       .collection("users")
-      .findOne({ _id: new mongodb.ObjectId(userId) });
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: new ObjectId(userId) });
   }
 }
 
