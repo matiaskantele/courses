6a0e50799413b14e1416a85b3224f171ecc09993
Matias Kantele <matias.kantele@siili.com>
2020-01-22 04:43:53 +0200

    delete products via mongodb

--- controllers/admin.js
+++ controllers/admin.js
@@ -79,13 +79,13 @@ exports.getProducts = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
-//     .then(product => product.destroy())
-//     .then(() => {
-//       console.log("Product Deleted!");
-//       res.redirect("/admin/products");
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log("Product Deleted!");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log(err));
+};
