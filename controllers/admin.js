e16b2b066d64c7c00abaf0da9d5df8d887a479c8
Matias Kantele <matias.kantele@siili.com>
2020-01-28 20:17:49 +0200

    authorize product creator to view/edit/delete it

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,21 +59,23 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDescription;
       product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(() => {
-      console.log('Product Updated!');
-      res.redirect('/admin/products');
+      return product.save().then(() => {
+        console.log('Product Updated!');
+        res.redirect('/admin/products');
+      });
     })
     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
@@ -89,7 +91,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('Product Deleted!');
       res.redirect('/admin/products');
