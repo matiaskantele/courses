c11787ce6369f2882c15671829b932e71c2eabc5
Matias Kantele <matias.kantele@siili.com>
2020-02-03 18:09:08 +0200

    serve product images statically

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
