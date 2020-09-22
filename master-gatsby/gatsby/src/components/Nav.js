2c71e395964ca4297e81d25652334feb95f56bad
Matias Kantele <matias.kantele@gmail.com>
2020-09-22 17:20:29 +0300

    style nav and add logo

--- master-gatsby/gatsby/src/components/Nav.js
+++ master-gatsby/gatsby/src/components/Nav.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 
+import Logo from './Logo';
+
+const StyledNav = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-of-type(1) {
+      --rotate: 1deg;
+    }
+    &:nth-of-type(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-of-type(4) {
+      --rotate: 1.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
+
 const links = [
-  { to: '/', name: 'Home' },
-  { to: '/beers', name: 'Beers' },
-  { to: '/order', name: 'Order' },
+  { to: '/', name: 'Hot Now' },
+  { to: '/pizzas', name: 'Pizza Menu' },
   { to: '/slicemasters', name: 'SliceMasters' },
-  { to: '/pizzas', name: 'Pizzas' },
+  { to: '/order', name: 'Order Ahead!' },
 ];
 
 const listItem = ({ to, name }) => (
@@ -16,9 +59,15 @@ const listItem = ({ to, name }) => (
 );
 
 const Nav = () => (
-  <nav>
-    <ul>{links.map((link) => listItem(link))}</ul>
-  </nav>
+  <StyledNav>
+    <ul>
+      {links.slice(0, 2).map((link) => listItem(link))}
+      <li>
+        <Logo />
+      </li>
+      {links.slice(2).map((link) => listItem(link))}
+    </ul>
+  </StyledNav>
 );
 
 export default Nav;
