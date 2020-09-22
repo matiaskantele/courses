0a45f805f118d4f48ecdce579dc75c9cf22dd54f
Matias Kantele <matias.kantele@gmail.com>
2020-09-22 16:51:30 +0300

    add typography

--- master-gatsby/gatsby/src/components/Footer.js
+++ master-gatsby/gatsby/src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Nav from './Nav';
-
 const Footer = () => (
   <footer>
     <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
--- master-gatsby/gatsby/src/components/Layout.js
+++ master-gatsby/gatsby/src/components/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 import Nav from './Nav';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
