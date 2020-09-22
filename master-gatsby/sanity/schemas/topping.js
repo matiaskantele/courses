78aa2afae3512f49529679398a4c7dc8e929c4b9
Matias Kantele <matias.kantele@gmail.com>
2020-09-22 23:57:28 +0300

    add topping schema

---
+++ master-gatsby/sanity/schemas/topping.js
@@ -0,0 +1,35 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+const toppingSchema = {
+  // computer name
+  name: 'topping',
+  // visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Can hipsters eat this?',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian && '🌱'}`,
+    }),
+  },
+};
+
+export default toppingSchema;
