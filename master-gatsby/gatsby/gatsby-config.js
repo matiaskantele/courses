f23773ca76a0eeb8b49ece921114293ae91d2021
Matias Kantele <matias.kantele@gmail.com>
2020-09-23 23:46:19 +0300

    add siteMetadata to gatsby

--- master-gatsby/gatsby/gatsby-config.js
+++ master-gatsby/gatsby/gatsby-config.js
@@ -1,1 +1,8 @@
-export const plugins = [`gatsby-plugin-emotion`];
+export default {
+  plugins: [`gatsby-plugin-emotion`],
+  siteMetadata: {
+    title: `Slicks Slices`,
+    siteUrl: `https://gatsby.pizza`,
+    description: `Best pizzas in Espoo`,
+  },
+};
