d2016bba98fd9c1c8fba55f979045831115e6b91
Matias Kantele <matias.kantele@gmail.com>
2020-09-23 22:16:43 +0300

    add person schema

---
+++ master-gatsby/sanity/schemas/person.js
@@ -0,0 +1,42 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+const personSchema = {
+  // computer name
+  name: 'person',
+  // visible title
+  title: 'SliceMasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Who dis?',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
+
+export default personSchema;
