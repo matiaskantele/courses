e1596324554debae9c9bcb6e9404b0bcf9b98de4
Matias Kantele <matias.kantele@gmail.com>
2020-09-25 12:49:03 +0300

    query pizzas from gatsby at build time

--- master-gatsby/gatsby/src/pages/pizzas.js
+++ master-gatsby/gatsby/src/pages/pizzas.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const Pizzas = () => (
-  <>
-    <p>Hey! This is the pizzas page.</p>
-  </>
-);
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        price
+      }
+    }
+    allSanityPerson {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+const Pizzas = ({ data }) => {
+  const pizzas = data.pizzas.nodes;
+  return (
+    <>
+      <p>Hey! There are {pizzas.length} pizzas!</p>
+    </>
+  );
+};
 
 export default Pizzas;
