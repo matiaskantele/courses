99b798729c067f59aa034098c38b3b81142c6c07
Matias Kantele <matias.kantele@gmail.com>
2020-09-25 12:29:02 +0300

    add sanity graphql api as gatsby data source

--- master-gatsby/gatsby/gatsby-config.js
+++ master-gatsby/gatsby/gatsby-config.js
@@ -1,5 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
-  plugins: [`gatsby-plugin-emotion`],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'zexpafm8',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `https://gatsby.pizza`,
