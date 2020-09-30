84978f924bdf0195532303a8e913d64da2032492
Matias Kantele <matias.kantele@gmail.com>
2020-09-30 23:07:46 +0300

    style pizza grid with subgrid

--- master-gatsby/gatsby/src/components/PizzaList.js
+++ master-gatsby/gatsby/src/components/PizzaList.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from '@emotion/styled';
+
+const StyledPizzaGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const StyledPizza = styled.div`
+  display: grid;
+  /* Take your row sizing from the parent grid */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const Pizza = ({ pizza }) => (
-  <Link to={`/pizza/${pizza.slug.current}`}>
-    <h2>
-      <span className="mark">{pizza.name}</span>
-    </h2>
+  <StyledPizza>
+    <Link to={`/pizza/${pizza.slug.current}`}>
+      <h2>
+        <span className="mark">{pizza.name}</span>
+      </h2>
+    </Link>
     <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
     <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
-  </Link>
+  </StyledPizza>
 );
 
 const PizzaList = ({ pizzas }) => (
-  <>
+  <StyledPizzaGrid>
     {pizzas.map((pizza) => (
       <Pizza key={pizza.id} pizza={pizza} />
     ))}
-  </>
+  </StyledPizzaGrid>
 );
 
 export default PizzaList;
