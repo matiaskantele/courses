6603f2bef46189507e851711d184d25c7296fb44
Matias Kantele <matias.kantele@gmail.com>
2020-10-31 22:11:30 +0200

    add slicemaster page

--- master-gatsby/gatsby/src/pages/slicemasters.js
+++ master-gatsby/gatsby/src/pages/slicemasters.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from '@emotion/styled';
 
-const SliceMasters = () => (
-  <>
-    <p>Hey! This is the slicemasters page.</p>
-  </>
-);
+const SliceMasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SliceMaster = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: -6rem 2rem 2rem 2rem;
+    position: relative;
+    z-index: 2;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const SliceMasters = ({
+  data: {
+    slicemasters: { nodes: slicemasters },
+  },
+}) => {
+  console.log(slicemasters);
+  return (
+    <>
+      <SliceMasterGrid>
+        {slicemasters.map((person) => (
+          <SliceMaster key={person.id}>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SliceMaster>
+        ))}
+      </SliceMasterGrid>
+      <p>Hey! This is the slicemasters page.</p>
+    </>
+  );
+};
 
 export default SliceMasters;
