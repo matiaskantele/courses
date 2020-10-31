58666af5de26901a848dfda8968b2071b3b22ee8
Matias Kantele <matias.kantele@gmail.com>
2020-10-31 21:52:04 +0200

    add page for displaying available wines
    There's 687 so this place is stocked!

--- master-gatsby/gatsby/src/pages/beers.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Beers = () => (
-  <>
-    <p>Hey! This is the beers page.</p>
-  </>
-);
-
-export default Beers;
---
+++ master-gatsby/gatsby/src/pages/wines.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import styled from '@emotion/styled';
+
+const WineGrid = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleWine = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    font-size: 10px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+  }
+`;
+
+export const query = graphql`
+  query {
+    allWine {
+      nodes {
+        id
+        wine
+        winery
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
+
+const Wines = ({
+  data: {
+    allWine: { nodes: wines },
+  },
+}) => {
+  console.log(wines);
+  return (
+    <>
+      <h2>{`We have ${wines.length} Wines available. Dine in Only!`}</h2>
+      <WineGrid>
+        {wines.map((wine) => {
+          const rating = Math.round(wine.rating.average);
+          return (
+            <SingleWine key={wine.id}>
+              <img src={wine.image} alt={wine.wine} />
+              <h3>{`${wine.winery} - ${wine.wine}`}</h3>
+              <p title={`${rating} out of 5 stars`}>
+                {`⭐`.repeat(rating)}
+                <span style={{ filter: `grayscale(100%)` }}>
+                  {`⭐`.repeat(5 - rating)}
+                </span>
+                <span>({wine.rating.reviews})</span>
+              </p>
+            </SingleWine>
+          );
+        })}
+      </WineGrid>
+    </>
+  );
+};
+
+export default Wines;
